ac5c94b3f2e031070a644d464ecc24217bd675da
Tanushree2007 <TanushreeITM080@gmail.com>
2024-10-28 22:03:24 +0530

    Add files via upload

---
+++ day25.cpp
@@ -0,0 +1,13 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+        int x;
+        int y;
+        cout << "enter the first no :";
+        cin >> x;
+        cout << "enter the second no :";
+        cin >> y;
+        int z = x - y ;
+        cout << "the difference between this no.s is :" <<z<<endl;
+}
