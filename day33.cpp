cd8334b5291b9b3c13cc39f988b7ce5983a663b9
Tanushree2007 <TanushreeITM080@gmail.com>
2024-11-05 18:32:03 +0530

    Add files via upload

---
+++ day33.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+main()
+{
+        int i=1;
+        while(i<=10)
+        {
+                cout<<i<<" "<<endl;
+                if(i==4)
+                i++;
+                break;
+        }
+}
