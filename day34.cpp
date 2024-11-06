edec9bdde1e30ddbafa9068ac68c5c97a99e37c2
Tanushree2007 <TanushreeITM080@gmail.com>
2024-11-06 21:07:25 +0530

    Add files via upload

---
+++ day34.cpp
@@ -0,0 +1,16 @@
+#include"iostream"
+using namespace std;
+float areaofcircle(float);
+main()
+{
+	float x,radius;
+	cout<<"enter radius";
+	cin>>radius;
+	x=area of circle(radius);
+	cout<<x;
+}
+float areaofcircle(float radius)
+{
+	float a=3.14*radius*radius;
+	return a;
+}
