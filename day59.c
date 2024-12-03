654cd88c2c034ccf50d0beee05d1559e259a321a
Tanushree2007 <TanushreeITM080@gmail.com>
2024-12-03 21:15:25 +0530

    Add files via upload

---
+++ day59.c
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-wid">
+        <link rel="stylesheet" type="text/css" href="html.css">
+        <title>ALL type of css</title>
+        <style>
+            h1{
+                color: green;    <!-- i am internal css-->    
+            }
+        </style>
+    </head>
+    <body>
+       <p style="color:red">Tanushree</p>  <!-- i am inline css--> 
+        <h1> weeeeeeeeeeee ye</h1>
+        <h4>we are in lit</h4>
+    </body>
+</html>
